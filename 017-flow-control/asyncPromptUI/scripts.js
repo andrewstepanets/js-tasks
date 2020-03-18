2a8c799932746bb2287c0ffb91a1017386614c38
Andrii Stepanets <ovrass@gmail.com>
2020-03-18 21:17:08 +0200

    Working with async await prompt

--- 017-flow-control/asyncPromptUI/scripts.js
+++ 017-flow-control/asyncPromptUI/scripts.js
@@ -33,6 +33,14 @@ function ask(options) {
       skipButton.textContent = 'cancel';
       popup.firstElementChild.appendChild(skipButton);
       // TODO: listen for a click on that cancel button
+      skipButton.addEventListener(
+        'click',
+        function() {
+          resolve(null);
+          destroyPopup(popup);
+        },
+        { once: true }
+      );
     }
     // listen for the submit event on the inputs
     popup.addEventListener(
@@ -60,11 +68,48 @@ function ask(options) {
 // select all buttons that have a question
 async function askQuestion(e) {
   const button = e.currentTarget;
+  const cancel = 'cancel' in button.dataset;
   console.log(button.dataset);
 
-  const answer = await ask({ title: button.dataset.question });
+  const answer = await ask({
+    title: button.dataset.question,
+    cancel,
+  });
   console.log(answer);
 }
 const buttons = document.querySelectorAll('[data-question]');
 
 buttons.forEach(button => button.addEventListener('click', askQuestion));
+
+const questions = [
+  { title: 'What is your name?' },
+  { title: 'What is your age?', cancel: true },
+  { title: 'What is your dogs name?' },
+];
+
+async function asyncMap(array, callback) {
+  // make an array to store our results
+  const results = [];
+  // loop over our array
+  for (const item of array) {
+    results.push(await callback(item));
+  }
+  // when we are done the loop, return it
+  return results;
+}
+
+async function go() {
+  const answers = await asyncMap(questions, ask);
+  console.log(answers);
+}
+
+go();
+
+// async function askMany() {
+//   for (const question of questions) {
+//     const answer = await ask(question);
+//     console.log(answer);
+//   }
+// }
+
+// askMany();
